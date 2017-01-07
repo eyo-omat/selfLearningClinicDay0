9529361ea1db4b63265fbc4ffcd0e65e21aac45b
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-07 11:54:29 +0100

    completed function for test cases

--- artiGeo/app/library.js
+++ artiGeo/app/library.js
@@ -9,22 +9,28 @@ aritGeo: function(arr){
 
   var arithSequence = arr[1] - arr[0];
   var geoSequence = arr[1] / arr[0];
-  var arith = false;
-  var geo = false;
+  var arithCount = 0;
+  var geoCount = 0;
 
   for (var i = 0; i < arr.length - 1; i++) {
-      if( arr[i + 1] - arr[i] === arithSequence )
-          arith = true;
-      if(arr[i + 1] / geoSequence === arr[i])
-          geo = true;
+      if( arr[i + 1] - arr[i] === arithSequence ){
+        arithCount++; //arith = true;
+      }          
+      if(arr[i + 1] / geoSequence === arr[i]){
+        geoCount++; //geo = true;
+      }
+          
   }
 
-  if(arith === true)
-    return "arithmetic";
-  else if(geo === true)
-    return "geometric";
-  else
+  if(arithCount === arr.length - 1){
+    return "Arithmetic";
+  }
+  else if(geoCount === arr.length - 1){
+    return "Geometric";
+  }
+  else{ 
     return -1;
+  }
 }
 
 
