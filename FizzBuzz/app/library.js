dccb677a6eef2a5d81112a1df333ec51e7f9bc63
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-07 10:33:46 +0100

    restructured fizzBuzz function for numbers divisible by 3 and 5

--- FizzBuzz/app/library.js
+++ FizzBuzz/app/library.js
@@ -3,12 +3,12 @@
 module.exports = {
 
     fizzBuzz: function(number){
-        if (number % 3 === 0) {
+        if ((number % 5 === 0) && (number % 3 === 0)) {
+            return "FizzBuzz";
+        } else if (number % 3 === 0) {
             return "Fizz";
         } else if (number % 5 === 0) {
             return "Buzz";
-        } else if ((number % 5 === 0) && (number % 3 === 0)) {
-            return "FizzBuzz";
         } else {
             return number;
         }
